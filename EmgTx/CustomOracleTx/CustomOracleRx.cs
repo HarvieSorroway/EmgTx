2c56c5b77c21311db0a7f020839cbc539f8089af
Harvie Sorroway <1934182494@qq.com>
2023-05-27 20:55:43 +0800

    修改了签名

--- EmgTx/CustomOracleTx/CustomOracleRx.cs
+++ EmgTx/CustomOracleTx/CustomOracleRx.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+
+
 namespace CustomOracleTx
 {
     /// <summary>
--- EmgTx/EmgTxCustom.cs
+++ EmgTx/EmgTxCustom.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using BepInEx;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
+
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+[BepInPlugin("emgtx", "EmgTx", "1.0.0")]
 public class EmgTxCustom
 {
     public static void Log(string msg)
